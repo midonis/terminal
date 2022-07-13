a8e26ac6ec2220ba357a89172764552c791eed1f
netbounty <filip.petrovic@netbounty.de>
2022-07-13 22:03:39 +0200

    linted code

--- src/Terminal.ts
+++ src/Terminal.ts
@@ -1,29 +1,25 @@
-import process from 'node:process';
+import process from "node:process";
 
 export class Terminal {
-	/**
-	 * Get the Terminal width.
-	 *
-	 * @returns number
-	 */
-	public static width(): number
-	{
-		if (! process.stdout.isTTY)
-			return 0;
+  /**
+   * Get the Terminal width.
+   *
+   * @returns number
+   */
+  public static width(): number {
+    if (!process.stdout.isTTY) return 0;
 
-		return process.stdout.getWindowSize()[0];
-	}
+    return process.stdout.getWindowSize()[0];
+  }
 
-	/**
-	 * Get the Terminal height.
-	 *
-	 * @returns number
-	 */
-	public static height(): number
-	{
-		if (! process.stdout.isTTY)
-			return 0;
+  /**
+   * Get the Terminal height.
+   *
+   * @returns number
+   */
+  public static height(): number {
+    if (!process.stdout.isTTY) return 0;
 
-		return process.stdout.getWindowSize()[1];
-	}
+    return process.stdout.getWindowSize()[1];
+  }
 }
--- tests/terminal.test.ts
+++ tests/terminal.test.ts
@@ -2,10 +2,10 @@ import { Terminal } from "../src/Terminal";
 
 describe("Terminal", () => {
   it("returns the terminal width", () => {
-    expect(typeof Terminal.width()).toBe('number');
+    expect(typeof Terminal.width()).toBe("number");
   });
 
   it("returns the terminal height", () => {
-    expect(typeof Terminal.height()).toBe('number');
+    expect(typeof Terminal.height()).toBe("number");
   });
 });
