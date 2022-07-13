b936c322f68b5e00f8714de3611dd2fa8c50087e
netbounty <filip.petrovic@netbounty.de>
2022-07-13 21:56:18 +0200

    added height() and width() methods

--- src/Terminal.ts
+++ src/Terminal.ts
@@ -1,5 +1,21 @@
+import process from 'node:process';
+
 export class Terminal {
-  public constructor() {
-    //
-  }
+	public static width(): number
+	{
+		if (process.stdout.isTTY) {
+			return process.stdout.getWindowSize()[0];
+		} else {
+			return 0;
+		}
+	}
+
+	public static height(): number
+	{
+		if (process.stdout.isTTY) {
+			return process.stdout.getWindowSize()[1];
+		} else {
+			return 0;
+		}
+	}
 }
