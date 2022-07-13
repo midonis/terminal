ad06c226eecac8fae1a4ce30b3adf2c6dbc6a718
netbounty <filip.petrovic@netbounty.de>
2022-07-13 21:58:28 +0200

    refactored Terminal.ts

--- src/Terminal.ts
+++ src/Terminal.ts
@@ -3,19 +3,17 @@ import process from 'node:process';
 export class Terminal {
 	public static width(): number
 	{
-		if (process.stdout.isTTY) {
-			return process.stdout.getWindowSize()[0];
-		} else {
+		if (! process.stdout.isTTY)
 			return 0;
-		}
+
+		return process.stdout.getWindowSize()[0];
 	}
 
 	public static height(): number
 	{
-		if (process.stdout.isTTY) {
-			return process.stdout.getWindowSize()[1];
-		} else {
+		if (! process.stdout.isTTY)
 			return 0;
-		}
+
+		return process.stdout.getWindowSize()[1];
 	}
 }
